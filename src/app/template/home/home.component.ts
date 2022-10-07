bc5b046ae2b03c4292da5bed495a8a9f2e9a5b3d
gutakeuchi <gustavo.takeuchi87@gmail.com>
2022-10-06 22:00:27 -0300

    aula do dia 06/10

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -7,7 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  nomeProduto: string = 'Curso de Angular';
+  anuncio: string = `O ${this.nomeProduto} está em promoção`;
+  idProduto: number = 123;
+  precoProduto: number = 2.59;
+  promocao: boolean = true;
+
+
+  constructor() {
+    // this.anuncio = 'O ' + this.nomeProduto + ' está em promoção';
+    console.log('Nome do Produto: ', this.nomeProduto);
+    console.log('Anuncio: ', this.anuncio);
+    console.log('Preço: ', this.precoProduto);
+    console.log('Promoção: ', this.promocao);
+    var variavel1;
+    let variavel2;
+    const variavel3 : number = 10;
+    //var idade = 15;
+    //console.log('Minha idade é: ', idade);
+    let idadeLet = 20;
+    console.log('Minha idade é: ', idadeLet);
+
+    /*
+    function imprimeIdade(){
+      var idade = 20;
+      console.log('Minha idade é: ', idade);
+    }
+    */
+
+
+    function imprimeIdade(){
+      let idade = 20;
+      console.log('Sua idade é: ', idade);
+    }
+
+    /*
+    function imprimeIdadeFor(){
+      for(var idade = 30; idade <= 40; idade++)
+      {
+        console.log('Idade dentro do for: ', idade);
+      }
+      console.log('Idade fora do for: ', idade);
+    }
+    */
+
+
+    let idade = 16;
+    function imprimeIdadeFor(){
+      for(let idade = 30; idade <= 40; idade++)
+      {
+        console.log('Idade dentro do for: ', idade);
+      }
+      console.log('Idade fora do for: ', idade);
+    }
+
+    imprimeIdade();
+    imprimeIdadeFor();
+  }
 
   ngOnInit(): void {
   }
