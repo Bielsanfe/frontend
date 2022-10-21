eb1805e117256726aef69654b95d3c0b5d5741b1
Bielsanfe <bielsanfe@gmail.com>
2022-10-20 22:33:03 -0300

    aula do dia 20/10/22

--- src/app/template/home/home.component.ts
+++ src/app/template/home/home.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
   nomeProduto: string = 'Curso de Angular';
   anuncio: string = `O ${this.nomeProduto} está em promoção`;
-  idProduto: number = 123;
-  precoProduto: number = 2.59;
-  promocao: boolean = true;
+  idProduto: number = 3;
+  precoProduto: number = 2.51;
+  promocao: boolean = false;
+  foto: string = 'assets/img/crud.png';
+  dataValidade = '2021-12-31'
 
 
   constructor() {
@@ -20,28 +21,27 @@ export class HomeComponent implements OnInit {
     console.log('Anuncio: ', this.anuncio);
     console.log('Preço: ', this.precoProduto);
     console.log('Promoção: ', this.promocao);
-    var variavel1;
+    /* var variavel1;
     let variavel2;
-    const variavel3 : number = 10;
+    const variavel3: number = 10;
     //var idade = 15;
     //console.log('Minha idade é: ', idade);
     let idadeLet = 20;
     console.log('Minha idade é: ', idadeLet);
 
-    /*
+
     function imprimeIdade(){
       var idade = 20;
       console.log('Minha idade é: ', idade);
     }
-    */
 
 
-    function imprimeIdade(){
+    function imprimeIdade() {
       let idade = 20;
       console.log('Sua idade é: ', idade);
     }
 
-    /*
+
     function imprimeIdadeFor(){
       for(var idade = 30; idade <= 40; idade++)
       {
@@ -49,7 +49,7 @@ export class HomeComponent implements OnInit {
       }
       console.log('Idade fora do for: ', idade);
     }
-    */
+
 
 
     let idade = 16;
@@ -63,9 +63,20 @@ export class HomeComponent implements OnInit {
 
     imprimeIdade();
     imprimeIdadeFor();
+
+
+    var a = 5;
+    var b = 10;
+    if(a = 5){
+      let a = 4;
+      var b = 1;
+      console.log('Variável a dentro do if: ', a);
+      console.log('Variável b dentro do if: ', b);
+    }
+    console.log('Variável a fora do if: ', a);
+    console.log('Variável b fora do if: ', b);
+    */
   }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
